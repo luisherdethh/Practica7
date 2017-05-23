000e707b2a991e182c57bd5d76d103891de33355
LuisHerdeth <lh.mendozav@gmail.com>
2017-05-23 16:46:12 +0200

    Initial commit

---
+++ src/DataBase.java
@@ -0,0 +1,9 @@
+/**
+ * Created by cuent on 23/05/2017.
+ */
+public class DataBase {
+    public DataBase(String server, String username, String password) throws Exception {
+    }
+    public void InsertPerson(String name) throws Exception {
+    }
+}
